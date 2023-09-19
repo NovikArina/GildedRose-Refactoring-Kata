c4aaf5c42438b84550cc7690d279b42bd04a66e2
ArinaSuhorukova <asuhorukova@remedypointsolutions.com>
2023-09-19 17:46:37 +0200

    Refactored GildedRose

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,57 +2,81 @@ package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
 
-    fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+    companion object {
+        const val AGED_BRIE = "Aged Brie"
+        const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+        const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+        const val CONJURED = "Conjured Mana Cake"
+    }
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+    // Aged Brie increases in Quality the older it gets
+    private fun updateAgedBrieItem(item: Item) {
+        item.sellIn--
+        item.quality++
+        validateQuality(item)
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
+    // Backstage passes increases in Quality as its SellIn value approaches
+    // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+    // Quality drops to 0 after the concert
+    private fun updateBackstagePassesItem(item: Item) {
+        item.sellIn--
+        if (item.sellIn < 0) {
+            item.quality = 0
+            return
+        }
+        item.quality++
+        if (item.sellIn <= 10) {
+            item.quality++
+        }
+        if (item.sellIn <= 5) {
+            item.quality++
+        }
+        validateQuality(item)
+    }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+    //Conjured items degrade in Quality twice as fast as normal
+    private fun updateConjuredItem(item: Item) {
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        item.sellIn--
+        item.quality -= 2
+        if (item.sellIn < 0) {
+            item.quality -= 2
+        }
+        validateQuality(item)
+    }
+
+    // Quality degrades twice as fast after sellIn date
+    private fun updateNormalItem(item: Item) {
+
+        item.sellIn--
+        item.quality--
+        if (item.sellIn < 0) {
+            item.quality--
+        }
+        validateQuality(item)
+    }
+
+    //The Quality of an item is never negative ane more than 50
+    private fun validateQuality(item: Item) {
+        if (item.quality < 0) {
+            item.quality = 0
+        }
+        if (item.quality > 50) {
+            item.quality = 50
         }
     }
 
+    fun updateQuality() {
+        for (item in items) {
+            when (item.name) {
+                AGED_BRIE -> updateAgedBrieItem(item)
+                BACKSTAGE_PASSES -> updateBackstagePassesItem(item)
+                SULFURAS -> {} // We don't need to change legendary Sulfuras items
+                CONJURED -> updateConjuredItem(item)
+                else -> updateNormalItem(item)
+            }
+        }
+    }
 }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,16 +2,119 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+//    @Test
+//    fun foo() {
+//        val items = listOf(Item("foo", 0, 0))
+//        val app = GildedRose(items)
+//        app.updateQuality()
+//        assertEquals("fixme", app.items[0].name)
+//
+//    }
 
+    @ParameterizedTest
+    @CsvSource(
+            "3, 10, 1, 13",
+            "7, 10, 1, 12",
+            "12, 10, 1, 11",
+            "1, 10, 2, 0",
+            "7, 10, 3, 18",
+    )
+    fun testBackstagePasses(
+            sellIn: Int,
+            quality: Int,
+            days: Int,
+            expectedQuality: Int
+    ) {
+        val items = listOf(Item(GildedRose.BACKSTAGE_PASSES, sellIn, quality))
+        val gildedRose = GildedRose(items)
+        for (i in 1..days) {
+            gildedRose.updateQuality()
+        }
+        assertEquals(expectedQuality, items[0].quality)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "2, 0, 5, 0",
+            "3, 50, 1, 48",
+            "2, 50, 3, 42",
+    )
+    fun testConjured(
+            sellIn: Int,
+            quality: Int,
+            days: Int,
+            expectedQuality: Int
+    ) {
+        val items = listOf(Item(GildedRose.CONJURED, sellIn, quality))
+        val gildedRose = GildedRose(items)
+        for (i in 1..days) {
+            gildedRose.updateQuality()
+        }
+        assertEquals(expectedQuality, items[0].quality)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "10, 20, 5, 25",
+            "0, 0, 3, 3",
+            "2, 50, 2, 50",
+    )
+    fun testAgedBrie(
+            sellIn: Int,
+            quality: Int,
+            days: Int,
+            expectedQuality: Int
+    ) {
+        val items = listOf(Item(GildedRose.AGED_BRIE, sellIn, quality))
+        val gildedRose = GildedRose(items)
+        for (i in 1..days) {
+            gildedRose.updateQuality()
+        }
+        assertEquals(expectedQuality, items[0].quality)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "3, 80, 1, 80",
+            "5, 80, 6, 80"
+    )
+    fun testSulfuras(
+            sellIn: Int,
+            quality: Int,
+            days: Int,
+            expectedQuality: Int
+    ) {
+        val items = listOf(Item(GildedRose.SULFURAS, sellIn, quality))
+        val gildedRose = GildedRose(items)
+        for (i in 1..days) {
+            gildedRose.updateQuality()
+        }
+        assertEquals(expectedQuality, items[0].quality)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "3, 40, 4, 35",
+            "5, 20, 3, 17",
+            "3, 3, 5, 0",
+    )
+    fun testNormalItems(
+            sellIn: Int,
+            quality: Int,
+            days: Int,
+            expectedQuality: Int
+    ) {
+        val items = listOf(Item("Normal", sellIn, quality))
+        val gildedRose = GildedRose(items)
+        for (i in 1..days) {
+            gildedRose.updateQuality()
+        }
+        assertEquals(expectedQuality, items[0].quality)
     }
 
 }
